7d23b63061ea9e9168c66ced2784a10ce42a272c
Akhil Bhasin <akhilbhasin1991@gmail.com>
2024-03-03 06:37:43 +0530

    model version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     implementation("com.google.firebase:firebase-crashlytics")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.firebaseui:firebase-ui-auth:8.0.2")
-    implementation("com.enigma.audiobook.models:audiobook-models:1.6-SNAPSHOT")
+    implementation("com.enigma.audiobook.models:audiobook-models:1.6.1-SNAPSHOT")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
 //    implementation(files("/Users/akhil/office-workspace/audiobook/audiobook/libs/audiobook-models-1.1-SNAPSHOT.jar"))
