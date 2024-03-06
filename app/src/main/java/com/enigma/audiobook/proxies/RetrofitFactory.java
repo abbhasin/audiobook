28c64ba5c605d5e38bb6d458aeecf204c8e6d3c7
Akhil Bhasin <akhilbhasin1991@gmail.com>
2024-03-06 05:54:11 +0530

    fix: ProgressBar inflataion failure on android 31(version 11) due to material design theme; add no dark mode, update button colors with new theme

--- app/src/main/java/com/enigma/audiobook/proxies/RetrofitFactory.java
+++ app/src/main/java/com/enigma/audiobook/proxies/RetrofitFactory.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class RetrofitFactory {
-    private static String BASE_URL = "http://192.168.1.7:8080/";
+    private static String BASE_URL = "https://5b94-2401-4900-1c54-7e66-c183-399c-cff9-4ffc.ngrok-free.app";
     private final Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(JacksonConverterFactory.create())
